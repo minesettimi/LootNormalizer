cd2eb88107d37f7da4a27984feefd6015e1547c4
minesettimi <minesettimi@gmail.com>
2026-09-25 14:48:14 -0400

    release

--- LootNormalizer.cs
+++ LootNormalizer.cs
@@ -1,5 +1,81 @@
-﻿namespace LootNormalizer;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using SPTarkov.Common.Models.Logging;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Helpers.Server;
+using SPTarkov.Server.Core.Models.Eft.Common;
+using SPTarkov.Server.Core.Models.Spt.Tables;
+using SPTarkov.Server.Core.Utils;
 
-public class Class1
+namespace LootNormalizer;
+
+[Injectable(TypePriority = OnLoadOrder.PostLoad + 3)]
+public class LootNormalizer(LocationTable locationTable, 
+    ModHelper modHelper,
+    JsonUtil jsonUtil,
+    ISptLogger<LootNormalizer> logger) : IOnLoad
 {
+    public readonly string ConfigPath = Path.Join(modHelper.GetAbsolutePathToModFolder(Assembly.GetExecutingAssembly()), "config.json");
+    public ModConfig ModConfig = null!;
+    
+    public async Task OnLoadAsync(CancellationToken cancellationToken)
+    {
+        ModConfig = await jsonUtil.DeserializeFromFileAsync<ModConfig>(ConfigPath, cancellationToken) ?? new ModConfig();
+
+        if (ModConfig.LogBase <= 1)
+        {
+            logger.Warning("[LootNormalizer] Invalid Log Base used. Use a value above 1.");
+            ModConfig.LogBase = 1.15;
+        }
+        
+        await File.WriteAllTextAsync(ConfigPath, jsonUtil.Serialize(ModConfig, true), cancellationToken);
+
+        Dictionary<string, Location> locationMap = locationTable.GetDictionary();
+        
+        foreach ((string locationId, Location location) in locationMap)
+        {
+            if (ModConfig.LocationBlacklist.Contains(locationId))
+                continue;
+            
+            location.LooseLoot?.AddTransformer(lootTransformer =>
+            {
+                AdjustLooseLoot(lootTransformer);
+
+                return lootTransformer;
+            });
+        }
+    }
+
+    private void AdjustLooseLoot(LooseLoot? looseLoot)
+    {
+        if (looseLoot == null || looseLoot.Spawnpoints == null)
+            return;
+        
+        foreach (Spawnpoint lootSpawn in looseLoot.Spawnpoints)
+        {
+            if (lootSpawn.ItemDistribution == null)
+                continue;
+            
+            foreach (LooseLootItemDistribution itemDistribution in lootSpawn.ItemDistribution)
+            {
+                if (itemDistribution.RelativeProbability < ModConfig.MinProbability || 
+                    itemDistribution.RelativeProbability == null)
+                    continue;
+
+                itemDistribution.RelativeProbability =
+                    Math.Max(Math.Round(Math.Log((itemDistribution.RelativeProbability ?? 0) + 1, ModConfig.LogBase)), 1);
+            }
+        }
+        
+        logger.Success("[LootNormalizer] Successfully normalized item relative probabilities.");
+    }
+}
+
+public record ModConfig
+{
+    [JsonPropertyName("logBase")] public double LogBase { get; set; } = 1.5;
+    [JsonPropertyName("minProbability")] public int MinProbability { get; set; } = 0;
+    [JsonPropertyName("locationBlacklist")] public List<string> LocationBlacklist { get; set; } = [];
 }
--- Metadata.cs
+++ Metadata.cs
@@ -1,6 +1,27 @@
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+using Version = SemanticVersioning.Version;
+
 namespace LootNormalizer;
 
-public class Metadata
+public static class Metadata
 {
-    
+    public record ModMetadata : IModMetadata
+    {
+        public string ModGuid { get; init; } = "com.minesettimi.lootnormalizer";
+        public string Name { get; init; } = "Loot Normalizer";
+        public string Author { get; init; } = "minesettimi";
+        public List<string>? Contributors { get; init; }
+
+        public Version Version { get; init; } = new(1, 0, 0);
+        public Range SptVersion { get; init; } = new("~4.1.5");
+
+        public bool HasPrepatcher { get; init; } = false;
+        
+        public List<string>? Incompatibilities { get; init; }
+        public Dictionary<string, Range>? ModDependencies { get; init; } = new();
+
+        public string? Url { get; init; } = "https://github.com/minesettimi/StrangeCurios";
+        public string License { get; init; } = "MIT";
+    }
 }
